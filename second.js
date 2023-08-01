c76e0e4c5690af539b2c95b48eaca225e013417f
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-01 19:08:55 +0530

    commented

--- second.js
+++ second.js
@@ -3,34 +3,46 @@
 let arr = [0, 1, 2, 3, 4, 5]
 
 console.log(arr.slice(1, 3));
-console.log(`after slice: ${arr}\n`);
+console.log(`after slice: ${arr}\n`); // slice counts *start to before given maximum limit*
 
 console.log(arr.splice(1, 3));
-console.log(`after splice: ${arr}\n`);
+console.log(`after splice: ${arr}\n`); // splice counts *start to end of limit*
 
 // 2.
 
 console.log("first");
-setTimeout(() => {
-    console.log("second");
-}, 0);
+
+setTimeout(() => {        // --|
+    console.log("second");//   |--> this runed at end of code
+}, 0);                    // --|
+
 console.log("third");
 
 // 3.
 
-var arr1 = "john".split('');
-var arr2 = arr1.reverse();
-var arr3 = "jones".split('');
-arr2.push(arr3);
+var arr1 = "john".split(''); // splits and converts into array
+var arr2 = arr1.reverse(); // arr1.reverse() effects arr1 and arr2,it also change both to n,h,o,j  
+var arr3 = "jones".split(''); // splits and converts into array
+arr2.push(arr3); // push(adds) arr3: j,o,n,e,s to arr2 and also affects arr1 because arr1.reverde()
 
-console.log(arr1);
-console.log(arr2);
+console.log(arr1); // output: [n,h,o,j, [j,o,n,e,s]]
+console.log(arr2); // output: [n,h,o,j, [j,o,n,e,s]]
 
-console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(0));
-console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(0));
+console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(0)); // array 1: length=5 last=n,h,o,j,j,o,n,e,s
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(0)); // array 2: length=5 last=n,h,o,j,j,o,n,e,s
 
 // 4.
 var a = "mihir modhwadiya".split('');
 var b = a.reverse();
 
 console.log(a);
+/*
+[
+  'a', 'y', 'i', 'd',
+  'a', 'w', 'h', 'd',
+  'o', 'm', ' ', 'r',
+  'i', 'h', 'i', 'm'
+]
+*/
+
+// 4.
