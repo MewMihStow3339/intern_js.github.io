df796006579f5437b4074fe888a4e97bdf95d241
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-01 18:46:54 +0530

    log-powerfulll

--- second.js
+++ second.js
@@ -7,3 +7,22 @@ console.log(`after slice: ${arr}\n`);
 
 console.log(arr.splice(1, 3));
 console.log(`after splice: ${arr}\n`);
+
+// 2.
+
+console.log("first");
+setTimeout(() => {
+    console.log("second");
+}, 0);
+console.log("third");
+
+var arr1 = "john".split('');
+var arr2 = arr1.reverse();
+var arr3 = "jones".split('');
+arr2.push(arr3);
+
+console.log(arr1);
+console.log(arr2);
+
+console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(0));
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(0));
