9a98068c62da29c290152350a91d0626b1df980c
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-03 16:10:41 +0530

    starting ass-3

--- ass/ass2/ass2.js
+++ ass/ass2/ass2.js
@@ -2,17 +2,19 @@ const buttons = document.getElementsByTagName("button");
 const result = document.getElementById("result");
 result.innerHTML = "0"
 let rplc;
+let flag = 0;
 const buttonPressed = (e) => {
-    if (!(e.target.id == '=' || e.target.id == 'AC' || e.target.id == 'C')) {
+    const condition = result.innerText.charAt(result.innerText.length - 1)
+    if (!(e.target.id == '0' || e.target.id == '=' || e.target.id == 'AC' || e.target.id == 'C' || e.target.id == '.')) {
         if (result.innerText == '0' || result.innerText == '/' || result.innerText == '-' || result.innerText == '+' || result.innerText == '*' || result.innerText == '%') {
             result.innerHTML = `${e.target.id}`;
         }
-        else if (result.innerText.charAt(result.innerText.length - 1) == '0' || result.innerText.charAt(result.innerText.length - 1) == '/' || result.innerText.charAt(result.innerText.length - 1) == '-' || result.innerText.charAt(result.innerText.length - 1) == '+' || result.innerText.charAt(result.innerText.length - 1) == '*' || result.innerText.charAt(result.innerText.length - 1) == '%') {
-            if ((e.target.id == '0' || e.target.id == '/' || e.target.id == '+' || e.target.id == '*' || e.target.id == '%'|| e.target.id == '-')) {
+        else if (condition == '0' || condition == '/' || condition == '-' || condition == '+' || condition == '*' || condition == '%') {
+            if ((e.target.id == '0' || e.target.id == '/' || e.target.id == '+' || e.target.id == '*' || e.target.id == '%' || e.target.id == '-')) {
                 rplc = result.innerHTML.replace(result.innerHTML.charAt(result.innerText.length - 1), `${e.target.id}`)
                 result.innerHTML = rplc;
             }
-            else{
+            else {
                 result.innerHTML += `${e.target.id}`;
             }
 
@@ -24,18 +26,50 @@ const buttonPressed = (e) => {
             result.innerHTML += `${e.target.id}`;
         }
     }
+    else if (e.target.id == '.') {
+        let updatedr = result.innerHTML.split("")
+        for (let el of updatedr) {
+            if (el == '.') {
+                flag = 1
+            }
+            else {
+                flag = 0
+            }
+            console.log(updatedr);
+            console.log(flag);
+        }
+        if (flag == 0) {
+            result.innerHTML += `${e.target.id}`;
+        } else {
+            result.innerHTML += "";
+        }
+    }
+    else if(e.target.id=='0'){
+        result.innerHTML += `${e.target.id}`
+    }
     else if (e.target.id == '=') {
-        result.innerHTML = eval(result.innerHTML)
+        if (!(condition == '/' || condition == '-' || condition == '+' || condition == '*' || condition == '%')) {
+            result.innerHTML = eval(result.innerHTML)
+        }
+        else {
+            result.innerHTML = result.innerHTML.replace(result.innerHTML.charAt(result.innerHTML.length - 1), "");
+        }
     }
     else if (e.target.id == 'AC') {
         result.innerHTML = '0'
     }
     else if (e.target.id == 'C') {
-        if (result.innerHTML != '0') {
+        if (result.innerHTML.charAt(0) != '0') {
             result.innerHTML = result.innerHTML.slice(0, -1)
+            if (result.innerHTML.length == 0) {
+                result.innerHTML = '0'
+            }
         }
-        else {
-
+        if (result.innerHTML.charAt(0) == '0') {
+            result.innerHTML = result.innerHTML.slice(0, -1)
+            if (result.innerHTML.length == 0) {
+                result.innerHTML = '0'
+            }
         }
     }
 
--- ass/ass3/ass3.js
+++ ass/ass3/ass3.js
@@ -1,20 +1,6 @@
-const buttons = document.getElementsByTagName("button");
-const result = document.getElementById("result");
-let str = "";
+function add() {
+    const input = document.getElementById("inp")
+    const content = input.innerText
 
-const buttonPressed = (e) => {
-    if (e.target.id != '=') {
-        console.log(e.target.innerText);
-        str += e.target.innerHTML;
-        result.innerHTML += `${e.target.id}`;
-    }
-    else if (e.target.id == '=') {
-        result.innerHTML = eval(result.innerHTML)
-    }
+    console.log(content);
 }
-
-for (let button of buttons) {
-    button.addEventListener("click", buttonPressed);
-}
-
-result.innerHTML = ""
