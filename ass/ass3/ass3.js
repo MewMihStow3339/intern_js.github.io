0104f4931cf202fb0bab5ce71b77dd1ecc983135
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-04 09:36:43 +0530

    idk 04-08

--- ass/ass3/ass3.js
+++ ass/ass3/ass3.js
@@ -2,7 +2,7 @@ let i = 1;
 let btni = 1;
 const add = () => {
     const newli = document.createElement("li")
-    newli.setAttribute("class", "btn-group")
+    newli.setAttribute("class", "btn-group col-md-6")
     newli.setAttribute("id", `li${i}`)
     document.querySelector("#ul").appendChild(newli)
 
@@ -18,19 +18,19 @@ const add = () => {
 
     const newbtn = document.createElement("button")
     newbtn.type = "button"
-    newbtn.innerHTML = "x"
-    newbtn.setAttribute("class", "btn btn-primary")
+    newbtn.innerHTML = "clear"
+    newbtn.setAttribute("class", "btn btn-danger")
     newbtn.setAttribute("id", `btn${btni}`)
     newbtn.setAttribute("onclick", `remove(li${i})`)
     document.querySelector(`#li${i}`).appendChild(newbtn)
 
-    const br = document.createElement("br")
-    document.querySelector(`#ul`).appendChild(br)
+    // const br = document.createElement("br")
+    // document.querySelector(`#ul`).appendChild(br)
     i++
     btni++
 }
 function remove(id) {
-    console.log(id);
+    // console.log(id);
     // i--
     // btni--
     // let el = document.getElementById(`li${i}`);
@@ -38,3 +38,7 @@ function remove(id) {
     // newli.removeChild(newli.lastElementChild)
     newli.removeChild(id)
 }
+function removeall(){
+    const removeallli = document.getElementById("ul")
+    removeallli.innerHTML = ""
+}   
