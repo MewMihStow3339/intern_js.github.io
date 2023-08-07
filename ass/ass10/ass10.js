3ca0e81ad7a18312138686ca690af4fc04bfaee9
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-07 13:37:23 +0530

    start formating and testing all ass

--- ass/ass10/ass10.js
+++ ass/ass10/ass10.js
@@ -1,9 +1,24 @@
 const disp = document.getElementById("disp");
+const alarmAudio = document.getElementById("alarm-audio");
+alarmAudio.src = "http://soundbible.com/grab.php?id=1252&type=mp3";
+let alaram;
 
+function aclock() {
+    alaram = document.getElementById("time").value
+}
 setInterval(() => {
     const date = new Date();
-    disp.innerHTML = date.toLocaleTimeString();
-},1000)
 
-alarmAudio.src = "http://soundbible.com/grab.php?id=1252&type=mp3";
-alarmAudio.load();
+    const currtime = date.toLocaleTimeString()
+    disp.innerHTML = currtime;
+
+    const atime = date.toLocaleTimeString(navigator.language, {
+        hour: 'numeric', minute: 'numeric', hour12: false
+    });
+
+    if (alaram == atime) {
+        alarmAudio.load();
+        alarmAudio.play();
+    }
+
+}, 1000)
