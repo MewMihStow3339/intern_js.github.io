d4fb3e23141a107d47fae1310e89b66a08712233
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-11 19:13:11 +0530

    remove arror

--- projects/pro2/invoice.js
+++ projects/pro2/invoice.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     let i = 1;
-    let removee = false;
+    let removeed = false;
+    let remove2 = false;
+    let incre = false;
+
     if (i == 1) {
         append(i)
         calc(i, 1)
         last_total(i)
 
         $(`#GST${i}, #Qua${i}, #Rate${i}`).on("input", function () {
+            console.log(i + "hello i am 2nd");
             calc(i, 1)
             last_total(i)
         })
@@ -22,25 +26,37 @@ $(document).ready(function () {
         })
 
         $(`#remove${i}`).click(function () {
-            i--
-            last_total(i)
-            $(this).closest('.row').remove();
-            console.log(i);
+            // if (i == 2) {
+                i = 0
+                last_total(i)
+                $(this).closest('.row').remove();
+                // if (i == 0) {
+                    removeed = true
+                // }
+                console.log(i);
+            // }
         })
 
         $(`#Upload`).click(function () {
             Upload(i)
         })
+        console.log(i + " hello i am 1st");
         i++
     }
 
     $("#add").click(function () {
-
+        remove2 = false
+        if (removeed == true) {
+            i = 1
+            removeed = false
+        }
+        console.log(i);
         append(i)
         calc(i, 1)
         last_total(i)
 
         $(`#GST${i}, #Qua${i}, #Rate${i}`).on("input", function () {
+
             calc(i, 1)
             last_total(i)
         })
@@ -57,6 +73,12 @@ $(document).ready(function () {
 
         $(`#remove${i}`).click(function () {
             i--
+            if (incre == false) {
+                i--
+                incre = true
+            }
+
+            remove2 = true
             $(this).closest('.row').remove();
             last_total(i)
             console.log(i);
@@ -65,8 +87,9 @@ $(document).ready(function () {
         $(`#Upload`).click(function () {
             Upload(i)
         })
-
-        i++
+        if (remove2 == false) {
+            i++
+        }
     })
 
     function append(i) {
@@ -75,7 +98,7 @@ $(document).ready(function () {
             <input type="text" class="form-control bg-transparent brdr-inp" value="Item Name" id="name${i}">
         </div>
         <div class="col d-flex align-items-center">
-            <input required type="text" class="form-control bg-transparent brdr-inp" id="HSN${i}" placeholder="#">
+            <input type="text" class="form-control bg-transparent brdr-inp" id="HSN${i}" placeholder="#">
             <div class="fnt">
                 <div class="fa fa-search"></div>
             </div>
@@ -99,25 +122,25 @@ $(document).ready(function () {
             <div class="fnt">
                 <div class="fa fa-rupee"></div>
             </div>
-            <input type="text" class="form-control bg-transparent brdr-inp" id="Amount${i}">
+            <input required type="text" class="form-control bg-transparent brdr-inp" id="Amount${i}">
         </div>
         <div class="col d-flex align-items-center">
             <div class="fnt">
                 <div class="fa fa-rupee"></div>
             </div>
-            <input disabled type="text" class="form-control bg-transparent brdr-inp" id="CGST${i}">
+            <input required disabled type="text" class="form-control bg-transparent brdr-inp" id="CGST${i}">
         </div>
         <div class="col d-flex align-items-center">
             <div class="fnt">
                 <div class="fa fa-rupee"></div>
             </div>
-            <input type="text" class="form-control bg-transparent brdr-inp" id="SGST${i}">
+            <input required disabled type="text" class="form-control bg-transparent brdr-inp" id="SGST${i}">
         </div>
         <div class="col d-flex align-items-center">
             <div class="fnt">
                 <div class="fa fa-rupee"></div>
             </div>
-            <input type="text" class="form-control bg-transparent brdr-inp" id="Total${i}">
+            <input required type="text" class="form-control bg-transparent brdr-inp" id="Total${i}">
         </div>
         <div class="col">
             <button type="button" class="fa fa-times btn btn-danger" id="remove${i}"></button>
@@ -188,36 +211,71 @@ $(document).ready(function () {
         $("#last_SGST").html(lst_SGST.toFixed(2));
         $("#last_total").html(lst_total.toFixed(2));
 
+        if (i == 0) {
+            $("#last_amount").html((0).toFixed(2));
+            $("#last_CGST").html((0).toFixed(2));
+            $("#last_SGST").html((0).toFixed(2));
+            $("#last_total").html((0).toFixed(2));
+        }
+
     }
 
+    let uploaded = false;
+    // let blank_input_executed2 = false;
+    let blank_input_executed = false;
 
     function Upload(i) {
-        for (let j = 1; j < i; j++) {
-            let iname = $(`#name${j}`).val()
-            let HSN = $(`#HSN${j}`).val()
-            let GST = $(`#GST${j}`).val()
-            let Qua = $(`#Qua${j}`).val()
-            let Rate = $(`#Rate${j}`).val()
-            $(`#Amount${j}`).val((Qua * Rate).toFixed(2))
-            let Amount = $(`#Amount${j}`).val()
-            $(`#CGST${j}`).val((((GST / 2) * $(`#Amount${j}`).val()) / 100).toFixed(2))
-            let CGST = $(`#CGST${j}`).val()
-            $(`#SGST${j}`).val((((GST / 2) * $(`#Amount${j}`).val()) / 100).toFixed(2))
-            let SGST = $(`#SGST${j}`).val()
-            $(`#Total${j}`).val(((+((GST * $(`#Amount${j}`).val()) / 100)) + (+$(`#Amount${j}`).val())).toFixed(2))
-            let Total = $(`#Total${j}`).val()
-            let obj = {
-                HSN: HSN,
-                GST: GST,
-                Qua: Qua,
-                Rate: Rate,
-                Amount: Amount,
-                CGST: CGST,
-                SGST: SGST,
-                Total: Total
+        if (i > 0 && uploaded == false) {
+            let parent_obj = []
+            for (let j = 1; j < i; j++) {
+                let iname = $(`#name${j}`).val()
+                let HSN = $(`#HSN${j}`).val()
+                let GST = $(`#GST${j}`).val()
+                let Qua = $(`#Qua${j}`).val()
+                let Rate = $(`#Rate${j}`).val()
+                $(`#Amount${j}`).val((Qua * Rate).toFixed(2))
+                let Amount = $(`#Amount${j}`).val()
+                $(`#CGST${j}`).val((((GST / 2) * $(`#Amount${j}`).val()) / 100).toFixed(2))
+                let CGST = $(`#CGST${j}`).val()
+                $(`#SGST${j}`).val((((GST / 2) * $(`#Amount${j}`).val()) / 100).toFixed(2))
+                let SGST = $(`#SGST${j}`).val()
+                $(`#Total${j}`).val(((+((GST * $(`#Amount${j}`).val()) / 100)) + (+$(`#Amount${j}`).val())).toFixed(2))
+                let Total = $(`#Total${j}`).val()
+                if ((iname == "" || iname == NaN || HSN == "" || HSN == NaN || GST == "" || GST == NaN || Qua == "" || Qua == NaN || Rate == "" || Rate == NaN || Amount == "" || Amount == NaN || CGST == "" || CGST == NaN || SGST == "" || SGST == NaN || Total == "" || Total == NaN) && blank_input_executed == false) {
+                    alert("Enter data in blank inputs")
+                    blank_input_executed = true;
+                    event.preventDefault()
+                    return
+                } else {
+                    event.preventDefault()
+                }
+                let obj = {
+                    item: iname,
+                    HSN: HSN,
+                    GST: GST,
+                    Qua: Qua,
+                    Rate: Rate,
+                    Amount: Amount,
+                    CGST: CGST,
+                    SGST: SGST,
+                    Total: Total
+                }
+                parent_obj.push(obj)
             }
-            const info = JSON.stringify(obj)
-            localStorage.setItem(iname, info)
+            if (blank_input_executed == false) {
+                const info = JSON.stringify(parent_obj)
+                localStorage.setItem(localStorage.length + 1, info)
+                uploaded = true;
+            } else {
+                event.preventDefault()
+            }
+        }
+        else if (i <= 0 && uploaded == false) {
+            alert("Please Enter info")
+            event.preventDefault()
+            return
+        } else {
+            event.preventDefault()
         }
     }
 
